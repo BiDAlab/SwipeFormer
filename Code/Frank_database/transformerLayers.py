020194d3f57773544b460f2ea2632c7d7654df2b
Paula Delgado-Santos <38189240+PaulaDelgado-Santos@users.noreply.github.com>
2024-05-06 08:27:11 +0200

    Update transformerLayers.py

--- Code/Frank_database/transformerLayers.py
+++ Code/Frank_database/transformerLayers.py
@@ -9,9 +9,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math, copy, time
 from torch.autograd import Variable
-# from layers.SelfAttention_Family import AttentionLayer, FullAttention, ProbAttention
-# from layers.AutoCorrelation import AutoCorrelation, AutoCorrelationLayer
-# from BlockRecurrentTransformer import BlockRecurrentAttention
 
 def clones(module, N):
     "Produce N identical layers."
@@ -243,28 +240,6 @@ class PositionalEncoding_for_BERT(nn.Module):
     def forward(self, x):
         #x = x + self.pe[:x.size(0), :]#Variable(self.pe[:x.size(0), :], requires_grad=False)
         return self.pe[:x.size(0), :]
-'''
-def make_model(src_vocab, tgt_vocab, N=6,
-               d_model=200, d_ff=800, h=8, dropout=0.1):
-    "Helper: Construct a model from hyperparameters."
-    c = copy.deepcopy
-    attn = MultiHeadedAttention(h, d_model)  # 多头attention
-    ff = PositionwiseFeedForward(d_model, d_ff, dropout)  # FFN
-    position = PositionalEncoding(d_model, dropout)  # 位置向量
-    model = EncoderDecoder(
-        Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
-        Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N),
-        nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
-        nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
-        Generator(d_model, tgt_vocab))
-    # This was important from their code.
-    # Initialize parameters with Glorot / fan_avg.
-    for p in model.parameters():
-        if p.dim() > 1:
-            nn.init.xavier_uniform(p)
-    return model
-    
-'''
 
 class BertEmbeddings(nn.Module):
     """Construct the embeddings from word, position and token_type embeddings.
@@ -465,7 +440,7 @@ class HAR_BLSTM(nn.Module):
         encoding = re
         return encoding.transpose(-1, -2)
     
-#class HAR_CNN_residualBlockChanged(nn.Module):
+class HAR_CNN_residualBlockChanged(nn.Module):
     "Implements CNN equation."
     def __init__(self, d_model, d_ff, filters, dropout=0.1):
         super(HAR_CNN_residualBlockChanged, self).__init__()
